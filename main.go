470baef2d161691e107bd30e00b20e44cab6032c
Jaqueline Abreu <jaquelineabreuvr@gmail.com>
2023-03-04 23:16:48 -0300

    criado o endpoint alunos

--- main.go
+++ main.go
@@ -4,9 +4,16 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
+func ExibeTodosAlunos(c *gin.Context){
+	c.JSON(200, gin.H{
+		"id":"1",
+		"nome":"Jaqueline Abreu",
+	})
+}
+
+func main() {	
 	
-	//subir servidor
 	r := gin.Default()
-	r.Run(":5000")
+	r.GET("/alunos",ExibeTodosAlunos)
+	r.Run(":8000")
 }
