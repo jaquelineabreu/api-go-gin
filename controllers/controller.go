d5fef709f8aeef5f4f8fc85490c49a777e82e1af
Jaqueline Abreu <jaquelineabreuvr@gmail.com>
2023-03-05 20:11:00 -0300

    criada lista de alunos para testar o endpoint alunos

--- controllers/controller.go
+++ controllers/controller.go
@@ -2,13 +2,11 @@ package controllers
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/jaquelineabreu/api-go-gin/models"
 )
 
 func ExibeTodosAlunos(c *gin.Context){
-	c.JSON(200, gin.H{
-		"id":"1",
-		"nome":"Jaqueline Abreu",
-	})
+	c.JSON(200, models.Alunos)
 }
 
 func Saudacao(c *gin.Context){
