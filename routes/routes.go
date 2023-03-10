e97032a9950b126e76559257a57cb3e51113c548
Jaqueline Abreu <jaquelineabreuvr@gmail.com>
2023-03-09 23:21:11 -0300

    finalizaçãodo CRUD

--- routes/routes.go
+++ routes/routes.go
@@ -14,6 +14,7 @@ func HandleRequests(){
 	r.POST("/alunos", controllers.CriaNovoAluno)
 	r.GET("/alunos/:id", controllers.BuscaAlunoPorID)
 	r.DELETE("/alunos/:id", controllers.DeletaAluno)
-
+	r.PATCH("alunos/:id", controllers.EditaAluno)
+	r.GET("/alunos/cpf/:cpf", controllers.BuscaAlunoPorCPF)
 	r.Run(":8000")
 }
