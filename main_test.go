4bccb1a7eded859f6fbb15d3b4bac89fd86ef6ed
Jaqueline Abreu <jaquelineabreuvr@gmail.com>
2023-03-15 17:49:36 -0300

    Teste da busca por ID

--- main_test.go
+++ main_test.go
@@ -9,21 +9,24 @@ import(
 	"net/http/httptest"
 	"github.com/stretchr/testify/assert"
 	"io/ioutil"
-	"fmt"
 	"github.com/jaquelineabreu/api-go-gin/models"
+	"strconv"
+	"fmt"
+	"encoding/json"
 ) 
 
 var ID int
 
 
 func RotasDeTeste() *gin.Engine{
+	gin.SetMode(gin.ReleaseMode)
 	rotas := gin.Default()
 
 	return rotas
 }
 
 func CriaAlunoMock(){
-	aluno := models.Aluno{Nome: "Nome do aluno Teste", CPF: "12345678901", RG:"098765432"}
+	aluno := models.Aluno{Nome: "Nome do aluno Test", CPF: "12345678901", RG:"098765432"}
 	database.DB.Create(&aluno)
 	ID = int(aluno.ID)
 }
@@ -61,4 +64,41 @@ func TestListandoAlunos(t *testing.T){
 	r.ServeHTTP(resposta, req)
 	assert.Equal(t, http.StatusOK, resposta.Code)
 	//fmt.Println(resposta.Body)
+}
+
+func TestBuscaAlunoPorCPF(t *testing.T){
+	database.ConectaComBancoDeDados()
+	CriaAlunoMock()
+	defer DeletaAlunoMock()
+	r := RotasDeTeste()
+	r.GET("/alunos/cpf/:cpf", controllers.BuscaAlunoPorCPF)
+	req, _ := http.NewRequest("GET", "/alunos/cpf/12345678901", nil)
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+	assert.Equal(t, http.StatusOK, resposta.Code)
+}
+
+func TestBuscaAlunoPorID(t *testing.T){
+	database.ConectaComBancoDeDados()
+	CriaAlunoMock()
+	defer DeletaAlunoMock()
+	r := RotasDeTeste()
+	r.GET("/alunos/:id", controllers.BuscaAlunoPorID)
+	pathDaBusca := "/alunos/" + strconv.Itoa(ID)
+	req, _ := http.NewRequest("GET", pathDaBusca, nil)
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+	assert.Equal(t, http.StatusOK, resposta.Code)
+
+	var alunoMock models.Aluno
+	json.Unmarshal(resposta.Body.Bytes(), &alunoMock)
+
+	assert.Equal(t, "Nome do aluno Teste", alunoMock.Nome)
+	assert.Equal(t, "12345678901", alunoMock.CPF)
+	assert.Equal(t, "098765432", alunoMock.RG)
+	assert.Equal(t, http.StatusOK, resposta.Code)
+
+
+	fmt.Println(alunoMock.Nome)
+
 }
